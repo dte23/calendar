2515260f66d06de041e21e3e5e6255dc3fa52d71
Stig U <sun004@uit.no>
2025-02-09 17:23:49 +0100

    UI optimization/fixes

--- app/src/main/java/com/example/calendar/MainActivity.kt
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -1,3 +1,4 @@
+
 package com.example.calendar
 
 import android.os.Bundle
@@ -20,8 +21,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.aspectRatio // possibly useless
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
@@ -46,6 +50,8 @@ import androidx.compose.ui.text.font.FontWeight
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.temporal.IsoFields
+import androidx.compose.ui.platform.LocalConfiguration
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -98,11 +104,16 @@ fun BackgroundImage(modifier: Modifier = Modifier) {
     )
 }
 
+
+// Date/Year input screen
+
+
 @Composable
 fun GetInput(onSubmit: (String, String) -> Unit) {
     var month by rememberSaveable { mutableStateOf("") }
     var year by rememberSaveable { mutableStateOf("") }
-    var errorMessage by rememberSaveable { mutableStateOf("") }
+    var monthError by rememberSaveable { mutableStateOf("") }
+    var yearError by rememberSaveable { mutableStateOf("") }
 
     val invalidMonthError = stringResource(R.string.invalid_month_error)
     val invalidYearError = stringResource(R.string.invalid_year_error)
@@ -110,87 +121,125 @@ fun GetInput(onSubmit: (String, String) -> Unit) {
     val enterYearLabel = stringResource(R.string.enter_year_label)
     val showCalendarButton = stringResource(R.string.show_calendar_button)
 
-    Column(
+    Box(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        contentAlignment = Alignment.Center
     ) {
-        OutlinedTextField(
+        // 'Box' surrounding the input fields/show calendar button
+        Box(
             modifier = Modifier
-                .background(color = Color.Black.copy(alpha = 0.3f))
-                .semantics { testTag = "monthInput" },
-            value = month,
-            onValueChange = { newValue ->
-                if (newValue.isEmpty() || newValue.toIntOrNull()?.let { it in 1..12 } == true) {
-                    month = newValue
-                    errorMessage = ""
-                } else {
-                    month = newValue
-                    errorMessage = invalidMonthError
+                .fillMaxWidth(0.9f)
+                .border(2.dp, Color.Black, shape = RoundedCornerShape(16.dp))
+                .background(Color.Black.copy(alpha = 0.5f), shape = RoundedCornerShape(16.dp))
+                .padding(24.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                // Month input field
+                OutlinedTextField(
+                    modifier = Modifier
+                        .semantics { testTag = "monthInput" },
+                    value = month,
+                    onValueChange = { newValue ->
+                        month = newValue
+                        monthError = if (newValue.isNotEmpty() && newValue.toIntOrNull()?.let { it in 1..12 } != true) {
+                            invalidMonthError
+                        } else {
+                            ""
+                        }
+                    },
+                    label = { Text(enterMonthLabel) },
+                    maxLines = 1,
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                    isError = monthError.isNotEmpty()
+                )
+
+                if (monthError.isNotEmpty()) {
+                    Text(
+                        text = monthError,
+                        color = Color.Red,
+                        style = MaterialTheme.typography.bodySmall,
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(start = 16.dp, top = 4.dp)
+                    )
                 }
-            },
-            label = { Text(enterMonthLabel) },
-            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-            isError = errorMessage.isNotEmpty()
-        )
 
-        Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
-            modifier = Modifier
-                .background(color = Color.Black.copy(alpha = 0.3f))
-                .semantics { testTag = "yearInput" },
-            value = year,
-            onValueChange = { newValue ->
-                if (newValue.isEmpty() || newValue.toIntOrNull() != null) {
-                    year = newValue
-                    errorMessage = ""
-                } else {
-                    year = newValue
-                    errorMessage = invalidYearError
+                // Year input field
+                OutlinedTextField(
+                    modifier = Modifier
+                        .semantics { testTag = "yearInput" },
+                    value = year,
+                    onValueChange = { newValue ->
+                        year = newValue
+                        yearError = if (newValue.isNotEmpty() && newValue.toIntOrNull() == null) {
+                            invalidYearError
+                        } else {
+                            ""
+                        }
+                    },
+                    label = { Text(enterYearLabel) },
+                    maxLines = 1,
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                    isError = yearError.isNotEmpty()
+                )
+                // Display year-specific error if any
+                if (yearError.isNotEmpty()) {
+                    Text(
+                        text = yearError,
+                        color = Color.Red,
+                        style = MaterialTheme.typography.bodySmall,
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(start = 16.dp, top = 4.dp)
+                    )
                 }
-            },
-            label = { Text(enterYearLabel) },
-            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-            isError = errorMessage.isNotEmpty()
-        )
 
-        if (errorMessage.isNotEmpty()) {
-            Text(
-                text = errorMessage,
-                color = Color.Red,
-                style = MaterialTheme.typography.displaySmall
-            )
+                Spacer(modifier = Modifier.height(160.dp))
+
+                // Grey out button unless valid inputs
+                Button(
+                    onClick = { onSubmit(month, year) },
+                    enabled = month.isNotEmpty() && year.isNotEmpty() && monthError.isEmpty() && yearError.isEmpty(),
+                    modifier = Modifier.semantics { testTag = "showCalendar" }
+                ) {
+                    Text(showCalendarButton)
+                }
+            }
         }
-
-        Spacer(modifier = Modifier.height(160.dp))
-
-        Button(
-            onClick = { onSubmit(month, year) },
-            enabled = month.isNotEmpty() && year.isNotEmpty() && errorMessage.isEmpty(),
-            modifier = Modifier.semantics { testTag = "showCalendar" }
-        ) { Text(showCalendarButton) }
     }
 }
 
+
+// Calendar screen
+
+
 @Composable
 fun DrawCalendar(month: Int, year: Int, onBack: () -> Unit) {
+    // Date calc
     val daysInMonth = YearMonth.of(year, month).lengthOfMonth()
+    //
     val firstDayOfMonth = (LocalDate.of(year, month, 1).dayOfWeek.value + 6) % 7
+    //
     var showDialog by rememberSaveable { mutableStateOf(false) }
     var daysSinceStartOfYear by rememberSaveable { mutableIntStateOf(0) }
 
+    // String resources.
     val daysPassedDialogTitle = stringResource(R.string.days_passed_dialog_title)
     val daysPassedMessage = stringResource(R.string.days_passed_message)
     val weekdays = listOf(
-        "",
+        "", // This first cell is reserved for the week number.
         stringResource(R.string.monday),
         stringResource(R.string.tuesday),
         stringResource(R.string.wednesday),
         stringResource(R.string.thursday),
         stringResource(R.string.friday),
         stringResource(R.string.saturday),
-        stringResource(R.string.sunday),
+        stringResource(R.string.sunday)
     )
     val months = listOf(
         stringResource(R.string.january),
@@ -204,8 +253,14 @@ fun DrawCalendar(month: Int, year: Int, onBack: () -> Unit) {
         stringResource(R.string.september),
         stringResource(R.string.october),
         stringResource(R.string.november),
-        stringResource(R.string.december),
+        stringResource(R.string.december)
     )
+
+    // Calculate a fixed cell size.
+    // We assume 16.dp padding on each side (total 32.dp) and 8 cells per row.
+    val configuration = LocalConfiguration.current
+    val totalHorizontalPadding = 32.dp
+    val cellSize = (configuration.screenWidthDp.dp - totalHorizontalPadding) / 8
 
     Column(
         modifier = Modifier
@@ -214,18 +269,33 @@ fun DrawCalendar(month: Int, year: Int, onBack: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Row {
+        // Month/Year header as a long bar.
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 0.6.dp)  // Band aid fixes for alignment problems
+                .padding(end = 0.6.dp)
+                .background(Color.Black.copy(alpha = 0.3f))
+                .border(1.dp, Color.Black)
+                .padding(vertical = 10.dp),   // Adjust vertical padding to taste.
+            contentAlignment = Alignment.Center
+        ) {
             Text(
                 text = "${months[month - 1]} $year",
-                style = MaterialTheme.typography.headlineSmall
+                style = MaterialTheme.typography.headlineSmall,
+                color = Color.White         // Use a contrasting color.
             )
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+       // Spacer(modifier = Modifier.height(8.dp))
+
+        // Weekdays header row.
         Row {
             weekdays.forEach { day ->
                 Box(
-                    modifier = Modifier.weight(1f).aspectRatio(1f).border(1.dp, Color.Black)
+                    modifier = Modifier
+                        .size(cellSize)           // Use fixed size.
+                        .border(1.dp, Color.Black)
                         .background(Color.Black.copy(alpha = 0.3f)),
                     contentAlignment = Alignment.Center
                 ) {
@@ -234,44 +304,56 @@ fun DrawCalendar(month: Int, year: Int, onBack: () -> Unit) {
             }
         }
 
+        // Calculate the total number of calendar cells and the number of rows.
         val totalCells = daysInMonth + firstDayOfMonth
         val rows = (totalCells / 7) + if (totalCells % 7 == 0) 0 else 1
 
         var dayCounter = 1
 
+        // Calendar grid
         Column {
             for (row in 0 until rows) {
-                val weekNumber = LocalDate.of(year, month, (dayCounter).coerceAtMost(daysInMonth))
-                    .get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)
-
+                // Determine the week number for the current row.
+                val weekNumber = if (dayCounter <= daysInMonth) {
+                    LocalDate.of(year, month, dayCounter).get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)
+                } else {
+                    0
+                }
                 Row {
+                    // Week number cell.
                     Box(
-                        modifier = Modifier.weight(1f).aspectRatio(1f).border(1.dp, Color.Black)
+                        modifier = Modifier
+                            .size(cellSize)
+                            .border(1.dp, Color.Black)
                             .background(Color.Black.copy(alpha = 0.3f)),
                         contentAlignment = Alignment.Center
                     ) {
-                        Text(text = weekNumber.toString(), fontWeight = FontWeight.Bold)
+                        if (weekNumber != 0) {
+                            Text(text = weekNumber.toString(), fontWeight = FontWeight.Bold)
+                        }
                     }
-
+                    // Seven cells for the days of the week
                     for (col in 0 until 7) {
                         val index = row * 7 + col
-                        val currentDay = dayCounter
                         Box(
                             modifier = Modifier
-                                .weight(1f).aspectRatio(1f).border(1.dp, Color.Black)
+                                .size(cellSize)
+                                .border(1.dp, Color.Black)
                                 .background(
-                                    if (index % 7 == 6) Color.Red.copy(alpha = 0.3f)
-                                    else Color.Black.copy(alpha = 0.3f))
-                                .semantics { testTag = "day_$currentDay" },
+                                    // red tint for sundays/holidays(nyi)
+                                    if (col == 6) Color.Red.copy(alpha = 0.3f)
+                                    else Color.Black.copy(alpha = 0.3f)
+                                )
+                                .semantics { testTag = "day_$dayCounter" },
                             contentAlignment = Alignment.Center
                         ) {
                             if (index >= firstDayOfMonth && dayCounter <= daysInMonth) {
                                 val currentDay = dayCounter
-
                                 Text(
                                     text = currentDay.toString(),
                                     modifier = Modifier.clickable {
-                                        daysSinceStartOfYear = calculateDaysSinceJanuaryFirst(currentDay, month, year)
+                                        daysSinceStartOfYear =
+                                            calculateDaysSinceJanuaryFirst(currentDay, month, year)
                                         showDialog = true
                                     }
                                 )
@@ -280,28 +362,43 @@ fun DrawCalendar(month: Int, year: Int, onBack: () -> Unit) {
                         }
                     }
                 }
-
             }
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = stringResource(R.string.workdays_message, calculateWorkDays(month, year)),
-            fontSize = 18.sp
-        )
+
+
+        // Display workdays message
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Black.copy(alpha = 0.3f))
+                .border(1.dp, Color.Black)
+                .padding(vertical = 10.dp)
+                .padding(start = 0.6.dp)  // Band aid fixes for alignment problems
+                .padding(end = 0.6.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = stringResource(R.string.workdays_message, calculateWorkDays(month, year)),
+                fontSize = 18.sp,
+                color = Color.White
+            )
+        }
 
         Spacer(modifier = Modifier.height(8.dp))
+
+        // Back button.
         Button(onClick = onBack) {
             Text("Back")
         }
     }
+
+    // Dialog to show days passed since Jan 1.
     if (showDialog) {
         AlertDialog(
             onDismissRequest = { showDialog = false },
             title = {
-                Text(
-                    text = daysPassedDialogTitle
-                )
+                Text(text = daysPassedDialogTitle)
             },
             text = {
                 Text(
@@ -317,6 +414,10 @@ fun DrawCalendar(month: Int, year: Int, onBack: () -> Unit) {
         )
     }
 }
+
+
+// Calculate workdays func
+
 
 fun calculateWorkDays(month: Int, year: Int): Int {
     val daysInMonth = YearMonth.of(year, month).lengthOfMonth()
